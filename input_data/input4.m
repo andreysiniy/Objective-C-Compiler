31d9ee8bbb8fc92ae85e2c63fc588a278042a41e
andreysiniy <kakavvmm@gmail.com>
2024-10-07 18:17:07 +0300

    added new input file

---
+++ input_data/input4.m
@@ -0,0 +1,25 @@
+
+int main () {
+  /* variable definition: */
+  int a, b;
+  int c;
+  float f;
+ 
+  /* aaaa
+	actual initialization 
+  bbbb */ 
+  a = 10;
+  b = 20;
+  // c = a + b
+  c = a + b;
+  NSLog(@"value of c : %d \n", c);
+
+  f = 70.0/3.0;
+  f = 12.5f;
+  unsigned int suff = 5u;
+  long long as = 23LL;
+  long long ab = 23lL;
+  NSLog(@"value of f : %f \\n", f);
+ 
+  return 0;
+}
