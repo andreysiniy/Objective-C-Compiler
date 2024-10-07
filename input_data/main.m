eed62dcbf11ffd416b2ff5c19d33ad0f431cf90f
andreysiniy <kakavvmm@gmail.com>
2024-10-07 13:46:46 +0300

    added another test file

---
+++ input_data/main.m
@@ -0,0 +1,10 @@
+#include <Foundation/Foundation.h>
+
+int main(void)
+{
+	//\\n mmm hello world
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    NSLog(@"Hello World!.");
+    [pool drain];
+ return;
+}
