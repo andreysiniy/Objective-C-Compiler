861d369567fba177ea73a8066eb5ac7f684c8622
andreysiniy <kakavvmm@gmail.com>
2025-01-04 20:40:37 +0300

    added functions and classes node

--- classes_nodes.cpp
+++ classes_nodes.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 long maxId = 0; // Глобальный id узла
 set<string> ClassNames; //Множество имен объявленных классов
+
 //---------- program ----------
 
 Program_node* Program_node::createProgramNode(Function_and_class_list_node* list)
@@ -11,4 +12,76 @@ Program_node* Program_node::createProgramNode(Function_and_class_list_node* list
     res->id = maxId++;
     res->list = list;
     return res;
+}
+
+// ---------- function_and_class_list ----------
+
+Function_and_class_list_node* Function_and_class_list_node::createFunctionAndClassListNodeFromFunction(Function_node *functionList)
+{
+    Function_and_class_list_node *res = new Function_and_class_list_node;
+    res->id = maxId++;
+    res->FunctionsAndClasses = new vector<function_and_class>;
+    struct function_and_class functionAndClass;
+    functionAndClass.function = functionList;
+    functionAndClass.class_block = NULL;
+    functionAndClass.class_declaration_list = NULL;
+    res->FunctionsAndClasses->push_back(functionAndClass);
+    return res;
+}
+
+Function_and_class_list_node* Function_and_class_list_node::createFunctionAndClassListNodeFromClassDeclarationList(Class_declaration_list_node *classDeclarationList)
+{
+    Function_and_class_list_node *res = new Function_and_class_list_node;
+    res->id = maxId++;
+    res->FunctionsAndClasses = new vector<function_and_class>;
+    struct function_and_class functionAndClass;
+    functionAndClass.class_declaration_list = classDeclarationList;
+    functionAndClass.class_block = NULL;
+    functionAndClass.function = NULL;
+    res->FunctionsAndClasses->push_back(functionAndClass);
+
+    return res;
+}
+
+Function_and_class_list_node* Function_and_class_list_node::createFunctionAndClassListNodeFromClassBlock(Class_block_node *classBlock)
+{
+    Function_and_class_list_node *res = new Function_and_class_list_node;
+    res->id = maxId++;
+    res->FunctionsAndClasses = new vector<function_and_class>;
+    struct function_and_class functionAndClass;
+    functionAndClass.class_block = classBlock;
+    functionAndClass.class_declaration_list = NULL;
+    functionAndClass.function = NULL;
+    res->FunctionsAndClasses->push_back(functionAndClass);
+    return res;
+}
+
+Function_and_class_list_node* Function_and_class_list_node::addToFunctionAndClassListNodeFromFunction(Function_and_class_list_node *list, Function_node *function)
+{
+    struct function_and_class functionAndClass;
+    functionAndClass.function = function;
+    functionAndClass.class_block = NULL;
+    functionAndClass.class_declaration_list = NULL;
+    list->FunctionsAndClasses->push_back(functionAndClass);
+    return list;
+}
+
+Function_and_class_list_node* Function_and_class_list_node::addToFunctionAndClassListNodeFromClassDeclarationList(Function_and_class_list_node *list, Class_declaration_list_node *classDeclarationList)
+{
+    struct function_and_class functionAndClass;
+    functionAndClass.class_declaration_list = classDeclarationList;
+    functionAndClass.class_block = NULL;
+    functionAndClass.function = NULL;
+    list->FunctionsAndClasses->push_back(functionAndClass);
+    return list;
+}
+
+Function_and_class_list_node* Function_and_class_list_node::addToFunctionAndClassListNodeFromClassBlock(Function_and_class_list_node *list, Class_block_node *classStatement)
+{
+    struct function_and_class functionAndClass;
+    functionAndClass.class_block = classStatement;
+    functionAndClass.class_declaration_list = NULL;
+    functionAndClass.function = NULL;
+    list->FunctionsAndClasses->push_back(functionAndClass);
+    return list;
 }
