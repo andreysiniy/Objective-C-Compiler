50e236905932081bdc90a5f28c59816cde726ac9
andreysiniy <kakavvmm@gmail.com>
2025-01-04 20:28:04 +0300

    added program node

--- classes_nodes.cpp
+++ classes_nodes.cpp
@@ -1,2 +1,14 @@
 #include "classes_nodes.h"
 #include <string>
+using namespace std;
+long maxId = 0; // Глобальный id узла
+set<string> ClassNames; //Множество имен объявленных классов
+//---------- program ----------
+
+Program_node* Program_node::createProgramNode(Function_and_class_list_node* list)
+{
+	Program_node *res = new Program_node;
+    res->id = maxId++;
+    res->list = list;
+    return res;
+}
