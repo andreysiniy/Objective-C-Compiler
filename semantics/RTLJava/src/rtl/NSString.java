a8a26cf3131fc7a5384773c1857271943947b089
andreysiniy <kakavvmm@gmail.com>
2025-01-15 19:05:58 +0300

    added basic NSString constructors

--- semantics/RTLJava/src/rtl/NSString.java
+++ semantics/RTLJava/src/rtl/NSString.java
@@ -3,4 +3,24 @@ package rtl;
 public class NSString extends  NSObject {
 
     private String string;
+
+    private NSString(String str) {
+        string = str;
+    }
+
+    private NSString(NSString str) {
+        string = str.string;
+    }
+
+    public static NSString stringStatic() {
+        return new NSString("");
+    }
+
+    public static NSString stringWithCStringStatic(char[] s) {
+        return new NSString(new String(s));
+    }
+
+    public static NSString stringWithStringStatic(NSString s) {
+        return new NSString(s);
+    }
 }
