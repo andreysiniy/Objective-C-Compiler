18ef5a180c6eb93235b4a192a081df81321df9f0
andreysiniy <kakavvmm@gmail.com>
2025-01-15 17:46:41 +0300

    added NSArray constructors

--- semantics/RTLJava/src/rtl/NSArray.java
+++ semantics/RTLJava/src/rtl/NSArray.java
@@ -6,4 +6,15 @@ import java.util.Arrays;
 public class NSArray extends NSObject {
 
     private NSObject[] array;
+    private NSArray() {
+        array = new NSObject[0];
+    }
+
+    private NSArray(NSArray array) {
+        this.array = array.array;
+    }
+
+    private NSArray(NSObject[] array) {
+        this.array = array;
+    }
 }
