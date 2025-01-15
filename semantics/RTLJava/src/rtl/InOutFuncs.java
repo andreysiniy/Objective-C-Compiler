e7ef7c73e76c23603e5446119d236325a7745860
andreysiniy <kakavvmm@gmail.com>
2025-01-15 17:30:13 +0300

    added InOut functions to RTL

--- semantics/RTLJava/src/rtl/InOutFuncs.java
+++ semantics/RTLJava/src/rtl/InOutFuncs.java
@@ -1,4 +1,41 @@
 package rtl;
 
 public class InOutFuncs {
+
+    //---------- Print Methods ----------
+    public static void printIntStatic(int i) {
+        System.out.println(i);
+    }
+
+    public static void printCharStatic(char c) {
+        System.out.println(c);
+    }
+
+    public static void printNSStringStatic(NSString s) {
+        System.out.println(s.toString());
+    }
+
+    public static void printCharArrayStatic(char[] s) {
+        System.out.println(new String(s));
+    }
+
+    public static void printObjectStatic(Object o) {
+        System.out.println(o);
+    } //TODO Возможно стоит заменить на NSObject
+
+    //---------- Read Methods ----------
+    /**
+     * Считывает строку с консоли
+     * */
+    public static String readStatic() {
+        return  System.console().readLine();
+    }
+
+
+    /**
+     * Считывает символ с консоли
+     * */
+    public static char readCharStatic() {
+        return  readStatic().charAt(0);
+    }
 }
