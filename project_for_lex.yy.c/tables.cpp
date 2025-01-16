0ca858272a7d9a8a9a803afa07711a3f84646725
andreysiniy <kakavvmm@gmail.com>
2025-01-16 16:55:29 +0300

    added constructors to constant table elements

--- project_for_lex.yy.c/tables.cpp
+++ project_for_lex.yy.c/tables.cpp
@@ -4,3 +4,22 @@
 #include <algorithm>
 #include "tables.h"
 using namespace std;
+
+// -------------------- ConstantsTableElement --------------------
+
+ConstantsTableElement::ConstantsTableElement(int id, constantType type, string utf8string)
+{
+	Id = id;
+	Type = type;
+	Utf8String = new string(utf8string);
+}
+
+
+ConstantsTableElement::ConstantsTableElement(int id, constantType type, int number, int firstRef, int secondRef)
+{
+	Id = id;
+	Type = type;
+	Number = number;
+	FirstRef = firstRef;
+	SecondRef = secondRef;
+}
