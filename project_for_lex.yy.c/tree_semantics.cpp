3917417ea2e7376470f8cb76d33ae973aa2c62f1
andreysiniy <kakavvmm@gmail.com>
2025-01-20 23:16:55 +0300

    fill constant tables for numeric and literal nodes

--- project_for_lex.yy.c/tree_semantics.cpp
+++ project_for_lex.yy.c/tree_semantics.cpp
@@ -1,3 +1,20 @@
 #include "tables.h"
 #include <algorithm>
 #include <string>
+// ---------- Numeric_constant ----------
+void Numeric_constant_node::fillLiterals(ConstantsTable* constantTable)
+{
+	if (Int > 32767 || Int < -32768) {
+		constantTable->findOrAddConstant(Integer, Int);
+	}
+}
+
+// ---------- literal ----------
+void Literal_node::fillLiterals(ConstantsTable* constantTable)
+{
+	if (type == STRING_CONSTANT_TYPE || type == NSSTRING_CONSTANT_TYPE) {
+		int utfIndex = constantTable->findOrAddConstant(UTF8, value);
+		constantTable->findOrAddConstant(String, 0, utfIndex);
+	}
+}
+
