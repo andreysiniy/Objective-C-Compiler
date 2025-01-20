41a019f63f126ef7410f06887052df6afb05a19c
andreysiniy <kakavvmm@gmail.com>
2025-01-20 23:25:50 +0300

    fill literals for init declarator node

--- project_for_lex.yy.c/tree_semantics.cpp
+++ project_for_lex.yy.c/tree_semantics.cpp
@@ -1,6 +1,18 @@
 #include "tables.h"
 #include <algorithm>
 #include <string>
+void Init_declarator_node::fillLiterals(ConstantsTable* constantTable)
+{
+	if (expression != NULL)
+		expression->fillLiterals(constantTable);
+	if (ArraySize != NULL)
+		ArraySize->fillLiterals(constantTable);
+	if (InitializerList != NULL)
+		InitializerList->fillLiterals(constantTable);
+}
+
+
+
 // ---------- Numeric_constant ----------
 void Numeric_constant_node::fillLiterals(ConstantsTable* constantTable)
 {
