e952d5ee85a48511f9ee622ae6d9d264abe24d65
andreysiniy <kakavvmm@gmail.com>
2025-01-20 23:39:47 +0300

    fill field/method refs for init declarator node

--- project_for_lex.yy.c/tree_semantics.cpp
+++ project_for_lex.yy.c/tree_semantics.cpp
@@ -1,6 +1,28 @@
 #include "tables.h"
 #include <algorithm>
 #include <string>
+// ---------- Init_declarator_node ---------- 
+void Init_declarator_node::fillFieldRefs(ConstantsTable* constantTable, LocalVariablesTable* localVariablesTable, ClassesTableElement* classTableElement)
+{
+	if (expression != NULL)
+		expression->fillFieldRefs(constantTable, localVariablesTable, classTableElement);
+	if (ArraySize != NULL)
+		ArraySize->fillFieldRefs(constantTable, localVariablesTable, classTableElement);
+	if (InitializerList != NULL)
+		InitializerList->fillFieldRefs(constantTable, localVariablesTable, classTableElement);
+}
+
+void Init_declarator_node::fillMethodRefs(ConstantsTable* constantTable, LocalVariablesTable* localVariablesTable, ClassesTableElement* classTableElement, bool isInInstanceMethod)
+{
+	if (expression != NULL)
+		expression->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+	if (ArraySize != NULL)
+		ArraySize->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+	if (InitializerList != NULL)
+		InitializerList->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+
+}
+
 void Init_declarator_node::fillLiterals(ConstantsTable* constantTable)
 {
 	if (expression != NULL)
