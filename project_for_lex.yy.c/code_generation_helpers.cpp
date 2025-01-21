92580ff37210daa0a98c6eb315812b5d272b1c97
andreysiniy <kakavvmm@gmail.com>
2025-01-22 02:48:45 +0300

    added const init in code gen helpers

--- project_for_lex.yy.c/code_generation_helpers.cpp
+++ project_for_lex.yy.c/code_generation_helpers.cpp
@@ -1,2 +1,7 @@
 ﻿#include "code_generation_helpers.h"
 
+//Инициализация констант
+char CodeGenerationHelpers::magicConstant[4] = { 0xCA, 0xFE, 0xBA, 0xBE };
+char CodeGenerationHelpers::smallVersion[2] = { 0x00, 0x00 };
+char CodeGenerationHelpers::bigVersion[2] = { 0x00, 0x3E };
+
