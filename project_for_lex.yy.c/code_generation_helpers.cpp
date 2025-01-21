ba1a83f59d6fc038412bc62828f6dd2d505d06bf
andreysiniy <kakavvmm@gmail.com>
2025-01-22 02:49:33 +0300

    conversion from int to byte array (vector<char>)

--- project_for_lex.yy.c/code_generation_helpers.cpp
+++ project_for_lex.yy.c/code_generation_helpers.cpp
@@ -5,3 +5,12 @@ char CodeGenerationHelpers::magicConstant[4] = { 0xCA, 0xFE, 0xBA, 0xBE };
 char CodeGenerationHelpers::smallVersion[2] = { 0x00, 0x00 };
 char CodeGenerationHelpers::bigVersion[2] = { 0x00, 0x3E };
 
+vector<char> CodeGenerationHelpers::intToByteArray(int num, int arraySize)
+{
+	vector<char> result(arraySize);
+	for (int i = 0; i < arraySize; i++)
+	{
+		result[arraySize - i - 1] = (num >> ( i * 8));
+	}
+	return result;
+}
