f769f20969804a29c5a015b38e038a73f5f2c4df
andreysiniy <kakavvmm@gmail.com>
2025-01-22 01:38:47 +0300

    added check for left value type in error handling

--- project_for_lex.yy.c/error_check.cpp
+++ project_for_lex.yy.c/error_check.cpp
@@ -1,2 +1,18 @@
 #include "tables.h"
 #include <string>
+
+void Expression_node::checkLvalueError()
+{
+	if (type == ASSIGNMENT_EXPRESSION_TYPE) {
+		bool isIncorrectLvalue = Left->type != IDENTIFIER_EXPRESSION_TYPE;
+		isIncorrectLvalue = isIncorrectLvalue && Left->type != SELF_EXPRESSION_TYPE;
+		isIncorrectLvalue = isIncorrectLvalue && Left->type != ASSIGNMENT_EXPRESSION_TYPE;
+		isIncorrectLvalue = isIncorrectLvalue && Left->type != ARROW_EXPRESSION_TYPE;
+		isIncorrectLvalue = isIncorrectLvalue && Left->type != ARRAY_ELEMENT_ACCESS_EXPRESSION_TYPE;
+
+		if (isIncorrectLvalue) {
+			string msg = "Incorrect lvalue. Lvalue have '" + Left->getTypeName() + "' type in line: " + to_string(line);
+			throw new std::exception(msg.c_str());
+		}
+	}
+}
