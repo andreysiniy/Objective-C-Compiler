4050ac92514f452edd89b451a62f33841daec4ad
andreysiniy <kakavvmm@gmail.com>
2025-01-21 17:30:42 +0300

    fill methodrefs for statement nodes

--- project_for_lex.yy.c/tree_semantics.cpp
+++ project_for_lex.yy.c/tree_semantics.cpp
@@ -1,6 +1,69 @@
 #include "tables.h"
 #include <algorithm>
 #include <string>
+void Statement_node::fillMethodRefs(ConstantsTable* constantTable, LocalVariablesTable* localVariablesTable, ClassesTableElement* classTableElement, bool isInInstanceMethod)
+{
+	if (type == EMPTY_STATEMENT_TYPE) {
+
+	}
+	else if (type == SIMPLE_STATEMENT_TYPE) {
+		if (Expression != NULL)
+			Expression->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+	}
+	else if (type == RETURN_STATEMENT_TYPE) {
+		if (Expression != NULL)
+			Expression->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+	}
+	else if (type == IF_STATEMENT_TYPE) {
+		If_statement_node* cur = (If_statement_node*)this;
+		cur->Condition->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+		if (cur->TrueBranch != NULL)
+			cur->TrueBranch->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+		if (cur->FalseBranch != NULL)
+			cur->FalseBranch->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+	}
+	else if (type == WHILE_STATEMENT_TYPE) {
+		While_statement_node* cur = (While_statement_node*)this;
+		cur->LoopCondition->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+		if (cur->LoopBody != NULL)
+			cur->LoopBody->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+	}
+	else if (type == DO_WHILE_STATEMENT_TYPE) {
+		Do_while_statement_node* cur = (Do_while_statement_node*)this;
+		cur->LoopCondition->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+		if (cur->LoopBody != NULL)
+			cur->LoopBody->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+	}
+	else if (type == FOR_STATEMENT_TYPE) {
+		For_statement_node* cur = (For_statement_node*)this;
+		if (cur->InitExpression != NULL)
+			cur->InitExpression->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+		if (cur->ConditionExpression != NULL)
+			cur->ConditionExpression->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+		if (cur->LoopExpression != NULL)
+			cur->LoopExpression->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+		if (cur->InitList != NULL)
+			cur->InitList->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+		if (cur->LoopBody != NULL)
+			cur->LoopBody->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+	}
+	else if (type == COMPOUND_STATEMENT_TYPE) {
+		Statement_list_node* cur = (Statement_list_node*)this;
+		if (cur->First != NULL) {
+			Statement_node* elem = cur->First;
+			while (elem != NULL) {
+				elem->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+				elem = elem->Next;
+			}
+		}
+	}
+	else if (type == DECLARATION_STATEMENT_TYPE) {
+		Declaration_node* cur = (Declaration_node*)this;
+		if (cur->init_declarator_list != NULL)
+			cur->init_declarator_list->fillMethodRefs(constantTable, localVariablesTable, classTableElement, isInInstanceMethod);
+	}
+}
+
 void Statement_node::fillLiterals(ConstantsTable* constantTable)
 {
 	if (type == EMPTY_STATEMENT_TYPE) {
