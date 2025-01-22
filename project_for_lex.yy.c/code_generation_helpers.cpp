11a7838c8ce5ca217efdaf419dd905e4696f25e1
andreysiniy <kakavvmm@gmail.com>
2025-01-22 13:53:51 +0300

    array to byte vector

--- project_for_lex.yy.c/code_generation_helpers.cpp
+++ project_for_lex.yy.c/code_generation_helpers.cpp
@@ -5,6 +5,14 @@ char CodeGenerationHelpers::magicConstant[4] = { 0xCA, 0xFE, 0xBA, 0xBE };
 char CodeGenerationHelpers::smallVersion[2] = { 0x00, 0x00 };
 char CodeGenerationHelpers::bigVersion[2] = { 0x00, 0x3E };
 
+void CodeGenerationHelpers::appendArrayToByteVector(vector<char>* data, char array[], int arraySize)
+{
+	for (int i = 0; i < arraySize; i++)
+	{
+		data->push_back(array[i]);
+	}
+}
+
 vector<char> CodeGenerationHelpers::intToByteArray(int num, int arraySize)
 {
 	vector<char> result(arraySize);
@@ -534,6 +542,7 @@ vector<char> CodeGenerationCommands::invokestatic(int constant)
 	CodeGenerationHelpers::appendArrayToByteVector(&res, temp.data(), temp.size());
 	return res;
 }
+
 // ---------- ireturn ----------
 vector<char> CodeGenerationCommands::ireturn()
 {
