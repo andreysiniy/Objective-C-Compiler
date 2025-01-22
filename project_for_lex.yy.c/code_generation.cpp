0b10478e4284df6bfee35ccfb817a31932f0c561
andreysiniy <kakavvmm@gmail.com>
2025-01-22 17:33:23 +0300

    code gen for while stmt

--- project_for_lex.yy.c/code_generation.cpp
+++ project_for_lex.yy.c/code_generation.cpp
@@ -46,6 +46,37 @@ vector<char> Statement_node::generateCodeForIfStatement(bool isInsideClassMethod
 
 	return res;
 }
+vector<char> Statement_node::generateCodeForWhileStatement(bool isInsideClassMethod, ConstantsTable* constantsTable, LocalVariablesTable* locals)
+{
+	vector<char> res;
+
+	While_statement_node* while_stmt = (While_statement_node*)this;
+
+	vector<char> bodyBytes = while_stmt->LoopBody->generateCode(isInsideClassMethod, constantsTable, locals);
+	vector<char> gotoBytes = CodeGenerationCommands::goto_(bodyBytes.size());
+	CodeGenerationHelpers::appendArrayToByteVector(&res, gotoBytes.data(), gotoBytes.size());
+	CodeGenerationHelpers::appendArrayToByteVector(&res, bodyBytes.data(), bodyBytes.size());
+
+	vector<char> conditionBytes = while_stmt->LoopCondition->generateCode(isInsideClassMethod, constantsTable); //Условие
+	CodeGenerationHelpers::appendArrayToByteVector(&res, conditionBytes.data(), conditionBytes.size());
+
+	int offset = bodyBytes.size() + conditionBytes.size();
+	offset = -offset;
+	vector<char> ifBytes;
+	if (while_stmt->LoopCondition->DataType->isPrimitive()) {
+		ifBytes = CodeGenerationCommands::if_(CodeGenerationCommands::NE, offset);
+	}
+	else {
+		vector<char> aconst_null = CodeGenerationCommands::aconst_null(); //Загрузка null для сравнения объекта
+		CodeGenerationHelpers::appendArrayToByteVector(&res, aconst_null.data(), aconst_null.size());
+		ifBytes = CodeGenerationCommands::if_acmp(CodeGenerationCommands::NE, offset);
+	}
+
+	CodeGenerationHelpers::appendArrayToByteVector(&res, ifBytes.data(), ifBytes.size());
+	
+
+	return res;
+}
 // ---------- Expression ----------
 vector<char> Expression_node::generateCodeForPlus(bool isInsideClassMethod, ConstantsTable* constantsTable)
 {
