5e15e1d25a1cfeb8c461b4199799a92279d8960b
andreysiniy <kakavvmm@gmail.com>
2025-01-22 14:21:24 +0300

    code gen for expr list node

--- project_for_lex.yy.c/code_generation.cpp
+++ project_for_lex.yy.c/code_generation.cpp
@@ -5,3 +5,14 @@
 #include <string>
 
 using namespace std;
+vector<char> Expression_list_node::generateCode(bool isInsideClassMethod, ConstantsTable* constantsTable)
+{
+	vector<char> res;
+	Expression_node* cur = First;
+	while (cur != NULL) {
+		vector<char> expression = cur->generateCode(isInsideClassMethod, constantsTable);
+		CodeGenerationHelpers::appendArrayToByteVector(&res, expression.data(), expression.size());
+		cur = cur->Next;
+	}
+	return res;
+}
