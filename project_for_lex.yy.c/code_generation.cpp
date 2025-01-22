2bfa882d7d2431e4f3bdc267e2191caa51567f19
andreysiniy <kakavvmm@gmail.com>
2025-01-22 23:19:19 +0300

    constant table byte gen

--- project_for_lex.yy.c/code_generation.cpp
+++ project_for_lex.yy.c/code_generation.cpp
@@ -5,6 +5,69 @@
 #include <string>
 
 using namespace std;
+// -------------------- Таблица констант --------------------
+vector<char> ConstantsTable::generateBytes()
+{
+	vector <char> res;
+	for (auto iter = items.cbegin(); iter != items.cend(); ++iter) {
+		vector<char> bytes = iter->second->generateBytes();
+		CodeGenerationHelpers::appendArrayToByteVector(&res, bytes.data(), bytes.size());
+	}
+		
+	return res;
+}
+
+vector<char> ConstantsTableElement::generateBytes()
+{
+	vector<char> res;
+	
+	// Добавление типа константы
+	res.push_back(Type);
+
+	switch (Type)
+	{
+	case UTF8: {
+		vector<char> len = CodeGenerationHelpers::intToByteArray(Utf8String->length(), 2); //Размер строки в байтах
+		char *str = new char[Utf8String->length() + 1]; //Строка
+		strcpy(str, Utf8String->c_str());
+		CodeGenerationHelpers::appendArrayToByteVector(&res, len.data(), len.size());
+		CodeGenerationHelpers::appendArrayToByteVector(&res, str, Utf8String->length());
+		delete[] str;
+	}
+		break;
+	case Integer: {
+		vector<char> num = CodeGenerationHelpers::intToByteArray(Number, 4); //Число в байтах
+		CodeGenerationHelpers::appendArrayToByteVector(&res, num.data(), num.size());
+	}
+		break;
+	case String:
+	case Class: {
+		vector<char> ref = CodeGenerationHelpers::intToByteArray(FirstRef, 2); //Номер константы UTF-8
+		CodeGenerationHelpers::appendArrayToByteVector(&res, ref.data(), ref.size());
+	}
+		break;
+	case NameAndType: {
+		vector<char> firstRef = CodeGenerationHelpers::intToByteArray(FirstRef, 2); //Номер константы UTF-8 с именем
+		vector<char> secondRef = CodeGenerationHelpers::intToByteArray(SecondRef, 2); //Номер константы UTF-8 с типом
+		CodeGenerationHelpers::appendArrayToByteVector(&res, firstRef.data(), firstRef.size());
+		CodeGenerationHelpers::appendArrayToByteVector(&res, secondRef.data(), secondRef.size());
+	}
+		break;
+	case FieldRef:
+	case MethodRef: {
+		vector<char> firstRef = CodeGenerationHelpers::intToByteArray(SecondRef, 2); //Номер константы Class с классом
+		vector<char> secondRef = CodeGenerationHelpers::intToByteArray(FirstRef, 2); //Номер константы NameAndType с типом и именем
+		CodeGenerationHelpers::appendArrayToByteVector(&res, firstRef.data(), firstRef.size());
+		CodeGenerationHelpers::appendArrayToByteVector(&res, secondRef.data(), secondRef.size());
+	}
+		break;
+	default:
+		break;
+	}
+
+	return res;
+}
+
 // -------------------- Таблица полей --------------------
 vector<char> FieldsTable::generateBytes()
 {
