1148d187c8314a5f1593d5bc5576078ab05fb63d
andreysiniy <kakavvmm@gmail.com>
2025-01-24 11:21:45 +0300

    added static function test

---
+++ project_for_lex.yy.c/tests/staticTest.m
@@ -0,0 +1,36 @@
+@class InOutFuncs;
+@implementation MyClass1 : NSObject
++ (void) print {
+    [InOutFuncs printCharArray: "call method print from class 'MyClass1'"];
+}
+@end
+
+//@implementation MyClass2 : MyClass1
+//- (void) print {
+//    [InOutFuncs printCharArray: "call method print from class 'MyClass2'"];
+//}
+//@end
+
+//@implementation MyClass3 : MyClass1
+//- (void) print {
+//    [InOutFuncs printCharArray: "call method print from class 'MyClass3'"];
+//    [super print];
+//}
+//@end
+
+// @implementation MyClass4 : MyClass2
+//@end
+
+int main ()
+{
+  //  MyClass1 *obj1 = [MyClass1 new];
+  //  MyClass2 *obj2 = [MyClass2 new];
+  //  MyClass3 *obj3 = [MyClass3 new];
+    
+  //  MyClass1 *objects[] = { obj1, obj2, obj3, [MyClass4 new] };
+    
+    [MyClass1 print];
+    //for (int i=0; i<4; i = i + 1) {
+    //    [objects[i] print];
+    //}
+}
